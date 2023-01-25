ea05ee35ad3fb0b7d313b76a6117908aeec6bfa4
Senner007 <nielshtg@htomail.com>
2023-01-25 17:27:37 +0100

    comment

--- src/permutation.ts
+++ src/permutation.ts
@@ -1,17 +1,18 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 // type to get a union of arrays with all permutations of the members of a string union
-type Permutation<T, C= T> = [T] extends [never]
+type Permutation<StringUnionAll, StringUnion = StringUnionAll> = [StringUnionAll] extends [never]
   ? []
-  : C extends T
-    ? [C, ...Permutation<Exclude<T, C>>]
+  : StringUnion extends StringUnionAll
+    ? [StringUnion, ...Permutation<Exclude<StringUnionAll, StringUnion>>]
     : [];
 
 
 type permutation = Permutation<"A" | "B" | "C">  // ["A", "B", "C"] | ["A", "C", "B"] | ["B", "A", "C"] | ["B", "C", "A"] | ["C", "A", "B"] | ["C", "B", "A"]
 
 // Explanation:
-// The condition "C extends T..." is in fact looping over all union members so that the conditional is first called with "'A' extends T..." then "'B' extends T" and so on.
+// The condition "StringUnion extends StringUnionAll..." is in fact looping over all union members so that the conditional is first called with "'A' extends T..." then "'B' extends T" and so on.
+// The purpose of the conditional is therefore to loop over all members of the string union, not to evaluate if StringUnion extends StringUnionAll. "StringUnion extends string..." would return the same result
 // Therefore it returns 3 arrays each with a nested union of arrays :
 type permutationEquivalence  = 
 | ["A", ...(["B" , "C"] | ["C" , "B"]) ] 
