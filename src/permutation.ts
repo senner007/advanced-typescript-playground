b78b4a5152cc9305693341395055a1403df8ca64
Senner007 <nielshtg@htomail.com>
2023-01-25 17:35:07 +0100

    more declarative

--- src/permutation.ts
+++ src/permutation.ts
@@ -1,18 +1,18 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 // type to get a union of arrays with all permutations of the members of a string union
-type Permutation<StringUnionAll, StringUnion = StringUnionAll> = [StringUnionAll] extends [never]
+type Permutation<StringUnionAll, StringUnionMember = StringUnionAll> = [StringUnionAll] extends [never]
   ? []
-  : StringUnion extends any
-    ? [StringUnion, ...Permutation<Exclude<StringUnionAll, StringUnion>>]
+  : StringUnionMember extends any
+    ? [StringUnionMember, ...Permutation<Exclude<StringUnionAll, StringUnionMember>>]
     : [];
 
 
 type permutation = Permutation<"A" | "B" | "C">  // ["A", "B", "C"] | ["A", "C", "B"] | ["B", "A", "C"] | ["B", "C", "A"] | ["C", "A", "B"] | ["C", "B", "A"]
 
 // Explanation:
-// The condition "StringUnion extends StringUnionAll..." is in fact looping over all union members so that the conditional is first called with "'A' extends T..." then "'B' extends T" and so on.
-// The purpose of the conditional is therefore to loop over all members of the string union, not to evaluate if StringUnion extends any
+// The condition "StringUnionMember extends StringUnionAll..." is in fact looping over all union members so that the conditional is first called with "'A' extends T..." then "'B' extends T" and so on.
+// The purpose of the conditional is therefore to loop over all members of the string union, not to evaluate if StringUnionMember extends any
 // Therefore it returns 3 arrays each with a nested union of arrays :
 type permutationEquivalence  = 
 | ["A", ...(["B" , "C"] | ["C" , "B"]) ] 
