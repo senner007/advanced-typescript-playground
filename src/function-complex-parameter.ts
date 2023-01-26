ca389079969d241e00294a654e5e6e5a9a8b506f
Senner007 <nielshtg@htomail.com>
2023-01-26 23:39:11 +0100

    refactor

--- src/function-complex-parameter.ts
+++ src/function-complex-parameter.ts
@@ -2,7 +2,7 @@
 type FirstAndLast<TArr_A extends readonly any[], TArr_B extends readonly any[]> = TArr_A extends readonly [infer First, ...infer Rest]
     ? TArr_B extends readonly [...infer Start, infer Last]
     ? First extends Last
-    ? { a: TArr_A, b: TArr_B }
+    ? [TArr_A, TArr_B]
     : First | Last | 'Error : First element does not extend last'
     : never
     : never
@@ -12,14 +12,14 @@ type t = FirstAndLast<readonly [1, 2, 3], readonly [1, 2, 1]>
 function func<
     TArr_A extends readonly any[],
     TArr_B extends readonly any[],
->(a: FirstAndLast<TArr_A, TArr_B>) {
-    return a
+>(...args: FirstAndLast<TArr_A, TArr_B>) {
+    return args
 }
 
 // Works
-// first element: 1 equals last element :1
-const works = func({ a: [1, 2, 3], b: [1, 1, 1]} as const)
+// first element: 1 equals last element : 1
+const works = func([1, 2, 3] as const, [1, 1, 1] as const)
 
 // Compile error
 // "Error : First element does not extend last" | 1 | 2
-const error = func({ a: [1, 2, 3], b: [1, 1, 2]} as const)
+const error = func([1, 2, 3] as const, [1, 1, 2] as const)
