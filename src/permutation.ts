b25fc8a67a1478c84e9715919cab25d92e9f0ce4
Senner007 <nielshtg@htomail.com>
2023-02-14 18:45:47 +0100

    tail recursive permutation

--- src/permutation.ts
+++ src/permutation.ts
@@ -7,6 +7,12 @@ type Permutation<StringUnionAll, StringUnionMember = StringUnionAll> = [StringUn
     ? [StringUnionMember, ...Permutation<Exclude<StringUnionAll, StringUnionMember>>]
     : [];
 
+// tail recursive optimized version
+type PermutationTailRecursive<StringUnionAll, Acc extends any[] = [], StringUnionMember = StringUnionAll> = [StringUnionAll] extends [never]
+  ? Acc
+  : StringUnionMember extends any
+  ? PermutationTailRecursive<Exclude<StringUnionAll, StringUnionMember>, [...Acc, StringUnionMember]>
+  : [];
 
 /*********************************************************************** */
 // Examples : 
@@ -18,16 +24,17 @@ type permutation = Permutation<"A" | "B" | "C">  // ["A", "B", "C"] | ["A", "C",
 // Explanation : 
 /*********************************************************************** */
 
-// The condition "StringUnionMember extends StringUnionAll..." is in fact looping over all union members so that the conditional is first called with "'A' extends T..." then "'B' extends T" and so on.
+// The condition "StringUnionMember extends any..." is in fact looping over all union members so that the conditional is first called with "'A' extends T..." then "'B' extends T" and so on.
 // The purpose of the conditional is therefore to loop over all members of the string union, not to evaluate if StringUnionMember extends any
 // Therefore it returns 3 arrays each with a nested union of arrays :
-type permutationEquivalence  = 
-| ["A", ...(["B" , "C"] | ["C" , "B"]) ] 
-| ["B", ...(["A" , "C"] |  ["C" , "A"])  ] 
-| ["C", ...(["A" , "B"] | ["B" , "A"])]
+type permutationEquivalence =
+  | ["A", ...(["B", "C"] | ["C", "B"])]
+  | ["B", ...(["A", "C"] | ["C", "A"])]
+  | ["C", ...(["A", "B"] | ["B", "A"])]
 // this is equal to all permutations: 
 type cases = [
-    Expect<Equal<permutation, permutationEquivalence>>,
+  Expect<Equal<permutation, permutationEquivalence>>,
+  Expect<Equal<permutation, PermutationTailRecursive<"A" | "B" | "C">>>,
 ]
 
 
