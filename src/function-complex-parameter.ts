8b7609106950b1bcfc887bb503a7c6130ed84d99
Senner007 <nielshtg@htomail.com>
2023-02-20 13:22:22 +0100

    update function complex parameters example

--- src/function-complex-parameter.ts
+++ src/function-complex-parameter.ts
@@ -1,6 +1,6 @@
 // type to ensure first element of first array extends last element of second array
-type FirstAndLast<TArr_A extends readonly any[], TArr_B extends readonly any[]> = TArr_A extends readonly [infer First, ...infer Rest]
-    ? TArr_B extends readonly [...infer Start, infer Last]
+type FirstAndLast<TArr_A extends readonly any[], TArr_B extends readonly any[]> = TArr_A extends readonly [infer First, ...any]
+    ? TArr_B extends readonly [...any, infer Last]
     ? First extends Last
     ? [TArr_A, TArr_B]
     : First | Last | 'Error : First element in first array does not extend last element in last array'
@@ -16,10 +16,18 @@ function func<
     return args
 }
 
+/*********************************************************************** */
+// Examples : 
+/*********************************************************************** */
+
 // Works
 // first element in first array equals last element in last array
-const works = func([1, 2, 3] as const, [1, 1, 1] as const)
+func([1, 2, 3] as const, [1, 1, 1] as const)
+func([{a : "3"}, 2, 3] as const, [1, 1, {a : "3"}] as const)
+func([[1,2,3,4], 2, 3] as const, [1, 1, [1,2,3,4]] as const)
 
 // Compile error
 // "Error : First element does not extend last" | 1 | 2
-const error = func([1, 2, 3] as const, [1, 1, 2] as const)
+func([1, 2, 3] as const, [1, 1, 2] as const)
+func([{a : "4"}, 2, 3] as const, [1, 1, {a : "3"}] as const)
+func([[1,2,3], 2, 3] as const, [1, 1, [1,2,3,4]] as const)
